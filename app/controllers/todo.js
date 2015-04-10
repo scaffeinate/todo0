ea9f3fe13aa64c8295417ae03caa6462439d661a
Sudharsanan <sudhar.ti@gmail.com>
2015-04-10 02:05:14 -0500

    Change tabs to spaces

--- app/controllers/todo.js
+++ app/controllers/todo.js
@@ -1,40 +1,40 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	isEditing: false,
-	state: function() {
-		return Ember.isEmpty(this.get('content').get('task'));
-	}.property('model.task'),
-	completed: function(key, value) {
-		var todo = this.get('model');
-		var _this = this;
-		if(value === undefined) {
-			return todo.get('completed');
-		} else {
-			todo.set('completed', value);
-			todo.save().then(function(){
-				if(value) {
-					_this.notify.info('task ' + todo.get('task') + ' completed');
-				}
-			});
-			return value;
-		}
-	}.property('model.completed'),
-	actions: {
-		editTodo: function() {
-			this.set('isEditing', true);
-		},
-		cancel: function() {
-			this.set('isEditing', false);
-			this.get('model').rollback();
-		},
-		updateTodo: function() {
-			var todo = this.get('content');
-			var _this = this;
-			todo.save().then(function(){
-				_this.notify.info('todo item updated');
-				_this.set('isEditing', false);
-			});
-		}
-	}
+  isEditing: false,
+  state: function() {
+    return Ember.isEmpty(this.get('content').get('task'));
+  }.property('model.task'),
+  completed: function(key, value) {
+    var todo = this.get('model');
+    var _this = this;
+    if(value === undefined) {
+      return todo.get('completed');
+    } else {
+      todo.set('completed', value);
+      todo.save().then(function(){
+        if(value) {
+          _this.notify.info('task ' + todo.get('task') + ' completed');
+        }
+      });
+      return value;
+    }
+  }.property('model.completed'),
+  actions: {
+    editTodo: function() {
+      this.set('isEditing', true);
+    },
+    cancel: function() {
+      this.set('isEditing', false);
+      this.get('model').rollback();
+    },
+    updateTodo: function() {
+      var todo = this.get('content');
+      var _this = this;
+      todo.save().then(function(){
+        _this.notify.info('todo item updated');
+        _this.set('isEditing', false);
+      });
+    }
+  }
 });
